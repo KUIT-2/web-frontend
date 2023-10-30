0a9fbb2c22974b8f660b593f5a527e3b8875fda4
hojune ji <ghwns8893@naver.com>
2023-10-30 22:47:35 +0900

    1

--- week6/ho0010/src/SearchBar.js
+++ week6/ho0010/src/SearchBar.js
@@ -1,10 +1,26 @@
 import React from "react";
 
-const SearchBar = () => {
+const SearchBar = ({ filterText, inStockOnly, onFilterTextChange, onInStockOnlyChange }) => {
+
+    const handleTextChange = (e) => {
+        onFilterTextChange(e.target.value);
+    }
+
+    const handleCheckboxChange = (e) => {
+        onInStockOnlyChange(e.target.checked);
+    }
+
     return <form>
-        <input type={"text"} placeholder="Search..." />
+        <input type={"text"}
+            placeholder="Search..."
+            value={filterText}
+            onChange={(e) => onFilterTextChange(e.target.value)}
+        />
         <lable>
-            <input type={"checkbox"} />
+            <input type={"checkbox"}
+                checked={inStockOnly}
+                onChange={handleCheckboxChange}
+            />
             Only show products in stock
         </lable>
     </form>
