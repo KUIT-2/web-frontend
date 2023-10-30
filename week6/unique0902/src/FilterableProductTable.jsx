a47291940e9c57c9820cd60d73ad9a04d3557ddd
unique0902 <rla0591@naver.com>
2023-10-30 18:45:48 +0900

    Feat: add week6 template to unique0902 folder

---
+++ week6/unique0902/src/FilterableProductTable.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import SearchBar from './SearchBar';
+import ProductTable from './ProductTable';
+import InputBar from './InputBar';
+
+const FilterableProductTable = ({ products, setProducts }) => {
+  const [filterText, setFilterText] = useState('');
+  const [inStockOnly, setInStockOnly] = useState(false);
+
+  const addProduct = (newProduct) => {
+    setProducts((previousData) => [...previousData, newProduct]);
+    // products = [{}, {}, {}, {}, ..., {}]
+    // newProducts = [...products, { newProduct }]
+  };
+
+  return (
+    <div>
+      <SearchBar
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+        onFilterTextChange={setFilterText}
+        onInStockOnlyChange={setInStockOnly}
+      />
+      <ProductTable
+        products={products}
+        filterText={filterText}
+        inStockOnly={inStockOnly}
+      />
+      <InputBar addProduct={addProduct} />
+    </div>
+  );
+};
+
+export default FilterableProductTable;
