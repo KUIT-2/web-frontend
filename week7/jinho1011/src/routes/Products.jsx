469fa85163d31778e81dfcd559a05eba7721b6b0
Jeon <gino9940@gmail.com>
2023-11-03 12:38:22 +0900

    feat: convert to jsx

---
+++ week7/jinho1011/src/routes/Products.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+const Products = () => {
+  const [products, setProducts] = useState([
+    { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
+    { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
+    { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+    { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
+    { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
+    { category: "Vegetables", price: "$1", stocked: true, name: "Peas" },
+  ]);
+
+  return (
+    <FilterableProductTable products={products} setProducts={setProducts} />
+  );
+};
+
+export default Products;
