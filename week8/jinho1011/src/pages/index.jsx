77ec2e91aa5304962fd0b5ffc6193bc7eeda8a98
Jeon <gino9940@gmail.com>
2023-11-08 23:04:55 +0900

    feat: add missing imports

--- week8/jinho1011/src/pages/index.jsx
+++ week8/jinho1011/src/pages/index.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+import Home from "../pages/Home";
+import Stores from "../pages/Stores";
+import Store from "../pages/Store";
+import Cart from "../pages/Cart";
+
 const Router = () => {
   const router = createBrowserRouter([
     {
