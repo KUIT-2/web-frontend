c155bc3634a99bcff6da4fe41c46bd6c3782d635
Jeon <gino9940@gmail.com>
2023-11-08 23:20:45 +0900

    feat: get storeId from params and data

--- week8/jinho1011/src/pages/Store.jsx
+++ week8/jinho1011/src/pages/Store.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+
+import stores from "../models/stores";
 
 const Store = () => {
-  return <div>Store</div>;
+  const { storeId } = useParams();
+
+  const store = stores.find((s) => s.id.toString() === storeId);
+
+  if (!store) {
+    return <div>가게를 찾을 수 없어요 🥺</div>;
+  }
+
+  return <div>{store.name}</div>;
 };
 
 export default Store;
