859c5a10c34ea93cc26056bea728ed7a2d85a418
Jeon <gino9940@gmail.com>
2023-11-09 03:07:42 +0900

    feat: implement store

--- week8/jinho1011/src/pages/Store/Store.jsx
+++ week8/jinho1011/src/pages/Store/Store.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
 import MenuItem from "../../components/MenuItem/MenuItem";
 import OrderBar from "../../components/OrderBar/OrderBar";
 
 import stores from "../../models/stores";
+import useCartStore from "../../store/cartStore";
 
 const Store = () => {
   const { storeId } = useParams();
+  const setStore = useCartStore((state) => state.setStore);
 
   const store = stores.find((s) => s.id.toString() === storeId);
+
+  useEffect(() => {
+    if (store) {
+      setStore(store);
+    }
+  }, []);
 
   if (!store) {
     return <div>가게를 찾을 수 없어요 🥺</div>;
