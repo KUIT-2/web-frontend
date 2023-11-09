842fe8d01a1375a3bec286d9bc8bbeb90fc67e61
unique0902 <rla0591@naver.com>
2023-11-09 15:17:25 +0900

    Feat: make home and products routes

--- week7/unique0902/src/App.js
+++ week7/unique0902/src/App.js
@@ -1,18 +1,11 @@
-import React, { useState } from 'react';
-import FilterableProductTable from './components/FilterableProductTable';
+import React from 'react';
+import Router from './routes/Router';
 
 const App = () => {
-  const [products, setProducts] = useState([
-    { category: 'Fruits', price: '$1', stocked: true, name: 'Apple' },
-    { category: 'Fruitsss', price: '$1', stocked: true, name: 'Dragonfruit' },
-    { category: 'Fruits', price: '$2', stocked: false, name: 'Passionfruit' },
-    { category: 'Vegetables', price: '$2', stocked: true, name: 'Spinach' },
-    { category: 'Vegetables', price: '$4', stocked: false, name: 'Pumpkin' },
-    { category: 'Vegetables', price: '$1', stocked: true, name: 'Peas' },
-  ]);
-
   return (
-    <FilterableProductTable products={products} setProducts={setProducts} />
+    <>
+      <Router />
+    </>
   );
 };
 
--- week7/unique0902/src/routes/Router.jsx
+++ week7/unique0902/src/routes/Router.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import App from '../App';
-import ErrorPage from '../ErrorPage';
+import ErrorPage from './ErrorPage';
+import Home from './Home';
+import Products from './Products';
 
 export default function Router() {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <App />,
+      element: <Home />,
       errorElement: <ErrorPage />,
+    },
+    {
+      path: '/products',
+      element: <Products />,
     },
   ]);
 
