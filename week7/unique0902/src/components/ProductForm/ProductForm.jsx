0456b7430b5d5dd371cb51b7f48e14ef58121a52
unique0902 <rla0591@naver.com>
2023-11-12 01:30:24 +0900

    Styles: add css to ProductForm

--- week7/unique0902/src/components/ProductForm/ProductForm.jsx
+++ week7/unique0902/src/components/ProductForm/ProductForm.jsx
@@ -26,33 +26,37 @@ const ProductForm = ({ product, handleSubmit }) => {
     <form className={styles.form}>
       <input
         type={'text'}
-        className={styles.input}
+        className={`${styles.input} ${styles.category}`}
         value={newProduct.category}
         onChange={(e) => handleChange(e.target.value, 'category')}
         placeholder='category...'
       />
       <input
         type={'text'}
-        className={styles.input}
+        className={`${styles.input} ${styles.price}`}
         value={newProduct.price}
         onChange={(e) => handleChange(e.target.value, 'price')}
         placeholder='price...'
       />
-      <label>Is Stocked</label>
-      <input
-        type={'checkbox'}
-        className={styles.input}
-        checked={newProduct.stocked}
-        onChange={(e) => handleChange(e.target.checked, 'stocked')}
-      />
+      <div className={styles.checkbox}>
+        <label>
+          Is Stocked
+          <input
+            type={'checkbox'}
+            className={`${styles.input} ${styles.stocked}`}
+            checked={newProduct.stocked}
+            onChange={(e) => handleChange(e.target.checked, 'stocked')}
+          />
+        </label>
+      </div>
       <input
         type={'text'}
-        className={styles.input}
+        className={`${styles.input} ${styles.name}`}
         value={newProduct.name}
         onChange={(e) => handleChange(e.target.value, 'name')}
         placeholder='name...'
       />
-      <button onClick={handleClickBtn} type={'button'}>
+      <button className={styles.btn} onClick={handleClickBtn} type={'button'}>
         {/* type=button이 없을 경우 버튼 누르면 새로고침됨 */}
         submit
       </button>
