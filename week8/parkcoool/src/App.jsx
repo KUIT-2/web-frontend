80e4fd82f04a0c332311a3d8d63c29facb51ba2e
parkcoool <yickle1345@naver.com>
2023-11-12 12:19:48 +0900

    feat: Pretendard 서체 적용

--- week8/parkcoool/src/App.jsx
+++ week8/parkcoool/src/App.jsx
@@ -1,10 +1,16 @@
 import Router from "./pages";
 
 function App() {
+    const style = {
+        margin: "0 auto",
+        width: "50vw",
+        minWidth: "320px",
+    };
+
     return (
-        <>
+        <div style={style}>
             <Router />
-        </>
+        </div>
     );
 }
 
