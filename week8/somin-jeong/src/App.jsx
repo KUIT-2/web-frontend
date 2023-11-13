799316eb87251cc35d434e0b25c811e53dc8b516
정소민 <yjk70702002@gmail.com>
2023-11-13 20:41:16 +0900

    feat: week8 lab

---
+++ week8/somin-jeong/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Router from "./pages";
+import { Normalize } from "styled-normalize";
+
+function App() {
+  return (
+    <>
+      <Normalize />
+      <Router />
+    </>
+  );
+}
+
+export default App;
---
+++ week8/somin-jeong/src/components/OrderBar/OrderBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import useCartStore from "../../store/cartStore";
+
+const OrderBar = () => {
+  const menus = useCartStore((state) => state.menus);
+  const store = useCartStore((state) => state.store);
+
+  const handleOrder = () => {};
+
+  return (
+    <div>
+      <div>총 주문금액</div>
+      <div>
+        {menus.reduce((acc, currentMenu) => acc + currentMenu.price, 0)}원
+      </div>
+      <button onClick={handleOrder} type="button">
+        {store?.name}에서 주문하기
+      </button>
+    </div>
+  );
+};
+
+export default OrderBar;
