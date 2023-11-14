5808c4b5adcfdab94ab5d2fc42db09a0b785044a
hyunn522 <tjgus52213@gmail.com>
2023-11-14 14:46:10 +0900

    feat: initial setting

---
+++ week8/hyunn522/src/pages/Store/Store.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react'
+import { useParams } from 'react-router-dom';
+
+import stores from "../../models/stores";
+import MenuItem from '../../components/MenuItem/MenuItem';
+import OrderBar from './../../components/OrderBar/OrderBar';
+import useCartStore from '../../store/cartStore';
+
+const Store = () => {
+  const { storeId } = useParams();
+  const setStore = useCartStore((state) => state.setStore);
+
+  const store = stores.find((s) => s.id.toString() === storeId);
+
+  useEffect(() => {
+    if(store) {
+      setStore(store);
+    }
+  },[])
+
+  if(!store) {
+    return <div>가게를 찾을 수 없어요 🥺</div>
+  }
+
+  return (
+    <div>
+      <h1>{store.name}</h1>
+      <div>
+        {store.menus.map((menu) => {
+          return <MenuItem key={menu.id} menu={menu} />;
+        })}
+      </div>
+      <OrderBar />
+    </div>
+  )
+}
+
+export default Store
