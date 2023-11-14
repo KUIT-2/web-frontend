6e4962e9c5885ef53cd54d037f45df16fe1410c4
unique0902 <rla0591@naver.com>
2023-11-14 22:47:37 +0900

    Fix: edit text of orderBar

--- week8/unique0902/src/components/OrderBar/OrderBar.tsx
+++ week8/unique0902/src/components/OrderBar/OrderBar.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import useCartStore from '../../api/cartStore';
 
 const OrderBar = () => {
   const menus = useCartStore((state) => state.menus);
   const store = useCartStore((state) => state.store);
-
-  const handleOrder = () => {};
+  const navigate = useNavigate();
+  const handleOrder = () => {
+    navigate('cart');
+  };
 
   return (
     <div>
@@ -14,7 +17,7 @@ const OrderBar = () => {
         {menus.reduce((acc, currentMenu) => acc + currentMenu.price, 0)}원
       </div>
       <button onClick={handleOrder} type='button'>
-        {store?.name}에서 주문하기
+        {store && store.name + '에서'} 주문하기
       </button>
     </div>
   );
