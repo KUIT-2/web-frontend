d0c962724cc59dc2b220029b61b64e36ca507f73
unique0902 <rla0591@naver.com>
2023-11-14 22:14:24 +0900

    Feat: add StoreListItem component

---
+++ week8/unique0902/src/components/StoreListItem/StoreListItem.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+type Props = {};
+
+const StoreListItem: React.FC = ({}: Props) => {
+  return (
+    <div>
+      <img src='' alt='' />
+      <div>
+        <p>1위</p>
+        <p>셀로리 한남점</p>
+        <p>✨4.9(3,919)</p>
+        <p>13분~30분.배달비 2,000원</p>
+      </div>
+    </div>
+  );
+};
+
+export default StoreListItem;
--- week8/unique0902/src/pages/Stores/Stores.tsx
+++ week8/unique0902/src/pages/Stores/Stores.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import StoreListItem from '../../components/StoreListItem/StoreListItem';
 
 const Stores = () => {
   return (
     <div>
       <button>{'<'}</button>
       <h2>샐러드</h2>
+      <StoreListItem />
+      <StoreListItem />
+      <StoreListItem />
     </div>
   );
 };
