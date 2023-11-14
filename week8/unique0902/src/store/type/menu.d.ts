c6fbfdc02ccef70daaa03e48cde02b1356d0897f
unique0902 <rla0591@naver.com>
2023-11-14 16:26:53 +0900

    Feat: make data types for menu, store

---
+++ week8/unique0902/src/store/type/menu.d.ts
@@ -0,0 +1,7 @@
+export type Menu = {
+  id: number;
+  name: string;
+  isBest: boolean;
+  price: number;
+  ingredients: string;
+};
---
+++ week8/unique0902/src/store/type/store.d.ts
@@ -0,0 +1,13 @@
+import { Menu } from './menu';
+
+export type Store = {
+  id: number;
+  name: string;
+  rate: number;
+  reviewCnt: number;
+  minDeliveryTime: number;
+  maxDeliveryTime: number;
+  minDeliveryPrice: number;
+  deliveryFee: number;
+  menus: Menu[];
+};
