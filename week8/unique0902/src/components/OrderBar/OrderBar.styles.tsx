28cac984c508321378be57c92db2a037f34c1b02
unique0902 <rla0591@naver.com>
2023-11-16 13:21:20 +0900

    Feat: make orderBar Component CSS

--- week8/unique0902/src/components/OrderBar/OrderBar.styles.tsx
+++ week8/unique0902/src/components/OrderBar/OrderBar.styles.tsx
@@ -1,1 +1,33 @@
-export {};
+import styled from 'styled-components';
+
+export const OrderFooter = styled.footer`
+  width: 100%;
+  box-sizing: border-box;
+  position: fixed;
+  background-color: #fff;
+  bottom: 0;
+  display: flex;
+  flex-direction: row;
+  padding: 16px 24px;
+  justify-content: space-between;
+  border-radius: 16px 16px 0px 0px;
+  align-items: center;
+  box-shadow: 0px -8px 16px 0px rgba(0, 0, 0, 0.1);
+`;
+export const OrderSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+export const OrderSectionTitle = styled.h2`
+  font-size: 15px;
+  font-weight: 400;
+  color: #6b7684;
+  margin: 0;
+`;
+export const OrderSectPrice = styled.p`
+  font-size: 17px;
+  font-weight: 600;
+  color: #4e5968;
+  margin: 0;
+`;
--- week8/unique0902/src/components/OrderBar/OrderBar.tsx
+++ week8/unique0902/src/components/OrderBar/OrderBar.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import useCartStore from '../../api/cartStore';
+import PrimaryBtn from '../Button/PrimaryBtn/PrimaryBtn';
+import {
+  OrderFooter,
+  OrderSection,
+  OrderSectionTitle,
+  OrderSectPrice,
+} from './OrderBar.styles';
 
 const OrderBar = () => {
   const menus = useCartStore((state) => state.menus);
@@ -11,15 +18,18 @@ const OrderBar = () => {
   };
 
   return (
-    <div>
-      <div>총 주문금액</div>
-      <div>
-        {menus.reduce((acc, currentMenu) => acc + currentMenu.price, 0)}원
-      </div>
-      <button onClick={handleOrder} type='button'>
+    <OrderFooter>
+      <OrderSection>
+        <OrderSectionTitle>총 주문금액</OrderSectionTitle>
+        <OrderSectPrice>
+          {menus.reduce((acc, currentMenu) => acc + currentMenu.price, 0)}원
+        </OrderSectPrice>
+      </OrderSection>
+
+      <PrimaryBtn handleClick={handleOrder}>
         {store && store.name + '에서'} 주문하기
-      </button>
-    </div>
+      </PrimaryBtn>
+    </OrderFooter>
   );
 };
 
