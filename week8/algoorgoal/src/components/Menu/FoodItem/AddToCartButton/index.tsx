689f11464dc55c779256fe24dc2459d3d61ca028
algoORgoal <qufstar46@gmail.com>
2023-11-18 17:05:02 +0900

    feat: add id prop

--- week8/algoorgoal/src/components/Menu/FoodItem/AddToCartButton/index.tsx
+++ week8/algoorgoal/src/components/Menu/FoodItem/AddToCartButton/index.tsx
@@ -2,14 +2,20 @@ import React from 'react';
 import Button from '../../../../common/components/Button';
 import Text from '../../../../common/components/Text';
 
-export default function AddToCartButton() {
+interface AddToCartButtonPropsType {
+  itemId: number;
+}
+
+export default function AddToCartButton({ itemId }: AddToCartButtonPropsType) {
   return (
     <Button
       type="button"
       color="primary"
       width="52px"
       height="32px"
-      onClick={() => {}}
+      onClick={() => {
+        console.log(itemId);
+      }}
     >
       <Text size="extraSmall" color="white">
         담기
