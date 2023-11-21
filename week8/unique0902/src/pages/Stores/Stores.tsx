e8cabbf4a600257ce6c25a0dfce5250b21f5de6c
unique0902 <rla0591@naver.com>
2023-11-21 19:18:04 +0900

    Feat: add store and menu img data

--- week8/unique0902/src/pages/Stores/Stores.tsx
+++ week8/unique0902/src/pages/Stores/Stores.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BackButton from '../../components/Button/BackButton/BackButton';
 import StoreListItem from '../../components/StoreListItem/StoreListItem';
-import stores from '../../models/stores';
+import { getStores } from '../../models/store';
+import { Store } from '../../store/type/store';
 import { PageTitle, PageTitleSect } from '../../styles/PageStyle';
 
 const Stores = () => {
+  const [stores, setStores] = useState<Store[] | undefined>();
+
+  useEffect(() => {
+    getStores().then((value) => setStores(value));
+  }, []);
+
   return (
     <React.Fragment>
       <header>
@@ -13,9 +20,8 @@ const Stores = () => {
       <PageTitleSect>
         <PageTitle>샐러드</PageTitle>
       </PageTitleSect>
-      {stores.map((store) => (
-        <StoreListItem store={store} key={store.id} />
-      ))}
+      {stores &&
+        stores.map((store) => <StoreListItem store={store} key={store.id} />)}
     </React.Fragment>
   );
 };
