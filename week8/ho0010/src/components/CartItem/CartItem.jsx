5656d6b04320d72dc169fe54fdc66a45b7795604
hojune ji <ghwns8893@naver.com>
2023-11-23 16:05:38 +0900

    Cart UI

--- week8/ho0010/src/components/CartItem/CartItem.jsx
+++ week8/ho0010/src/components/CartItem/CartItem.jsx
@@ -12,13 +12,12 @@ const CartItem = ({ menu }) => {
         <Item>
             <ItemImg />
             <ItemInf>
-                <h4>{menu.name}</h4>
-                <div>추천소스...</div>
-                <div>{menu.price}원</div>
-                <div>{menuCount}</div>
+                <ItemMenu>{menu.name}</ItemMenu>
+                <ItemOption>추천소스...</ItemOption>
+                <ItemPrice>{menu.price}원</ItemPrice>
             </ItemInf>
-            <ItemNum></ItemNum>
-            <div> &gt;</div>
+            <ItemNum> {menuCount}개</ItemNum>
+            <ItemOptionButton> &gt;</ItemOptionButton>
         </Item>
     )
 }
@@ -31,13 +30,50 @@ width : 54px;
 height : 54px;
 border-radius : 20%;
 display:flex;
+margin-left:20px;
+
 `
 const ItemInf = styled.div`
 display:flex;
 flex-direction : column;
-`
-const ItemNum = styled.div`
+margin-left:20px;
+gap:5px;
+
+
 `
 const Item = styled.div`
 display:flex;
+padding-bottom:20px;
+padding-top:20px;
+border-bottom: 1px solid #E5E8EB;
+
+
+`
+const ItemMenu = styled.div`
+font-weight:700;
+font-size:17px;
+`
+const ItemOption = styled.div`
+font-weight:500;
+font-size:13px;
+color:#6b7684;
+`
+const ItemPrice = styled.div`
+font-weight:500;
+font-size:13px;
+color:#6b7684;
+`
+const ItemNum = styled.div`
+display:flex;
+margin-left:auto;
+align-items:center;
+font-size:15px;
+color:#6b7684;
+`
+const ItemOptionButton = styled.button`
+background-color:#fff;
+border:none;
+cursor: pointer;
+margin:5px;
+
 `
--- week8/ho0010/src/pages/Cart/Cart.jsx
+++ week8/ho0010/src/pages/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavTop from "../../components/Nav/NavTop";
 import styled from "styled-components";
 import useCartStore from "../../store/cartStore";
@@ -24,6 +24,7 @@ const Cart = () => {
     navigate('/store');
   }
 
+
   if (!store) {
     return <div>메뉴를 담아주세요 🥺</div>;
   }
@@ -39,14 +40,13 @@ const Cart = () => {
       <CartMiddle>
         <EmptySpace />
         <StoreInf>
-
           <StoreInfTop>
             <StoreName>
               {store.name}
             </StoreName>
             <StoreMinPrice>
               {TotalPrice >= store.minDeliveryPrice ?
-                <p>주문가능합니다</p> : <p>최소금액 미달</p>}
+                <PriceSafe>주문가능</PriceSafe> : <PriceWarning>최소금액 미달</PriceWarning>}
             </StoreMinPrice >
           </StoreInfTop>
           <MenuInf>
@@ -55,22 +55,33 @@ const Cart = () => {
             })}
           </MenuInf>
         </StoreInf>
-        <InputMore onClick={handelInputMore}>더 담기 + </InputMore>
+        <ButtonContainer>
+        <InputMoreButton onClick={handelInputMore}>더 담기 + </InputMoreButton>
+        </ButtonContainer>
         <EmptySpace />
       </CartMiddle>
 
       <CartPrice>
-        <OrderPrice>주문금액 {MenuTotalPrice}</OrderPrice>
-        <DeliveryFee>배달요금 {store.deliveryFee}</DeliveryFee>
-        <CartTotalPrice>총 결제금액 {TotalPrice}</CartTotalPrice>
+        <OrderPriceContainer>
+          <OrderPriceText> 주문금액 </OrderPriceText>
+          <OrderPrice>{MenuTotalPrice}원</OrderPrice>
+        </OrderPriceContainer>
+        <DeliveryFeeContainer>
+          <DeliveryFeeText>배달요금 </DeliveryFeeText>
+          <DeliveryFee>{store.deliveryFee}원</DeliveryFee>
+        </DeliveryFeeContainer>
+        <CartTotalPriceContainer>
+          <CartTotalPriceText>총 결제금액</CartTotalPriceText>
+          <CartTotalPrice>{TotalPrice}원</CartTotalPrice>
+        </CartTotalPriceContainer>
       </CartPrice>
-
+      <CartBottomContainer>
       <CartBottom>
-        <MinPrice>최소 주문금액 {store.minDeliveryPrice}</MinPrice>
+        <MinPrice>최소 주문금액 {store.minDeliveryPrice}원</MinPrice>
 
         <TotalPriceButton>{TotalPrice}원 결제하기</TotalPriceButton>
       </CartBottom>
-
+      </CartBottomContainer>
 
     </div>
   )
@@ -90,7 +101,16 @@ margin:15px;
 font-weight:bold;
 cursor: pointer;
 `
-
+const PriceSafe = styled.div`
+color:#3182f6
+`
+const PriceWarning =styled.div`
+display:flex;
+margin:18px;
+color:#f04452;
+font-size:15px;
+font-weight:500;
+`
 const CartMiddle = styled.div`
 
 `
@@ -103,7 +123,17 @@ width:390px;
 const StoreInf = styled.div`
 
 `
-const InputMore = styled.button`
+const ButtonContainer=styled.div`
+display:flex;
+justify-content:center;
+`
+const InputMoreButton = styled.button`
+background-color:#fff;
+border:none;
+color:#3182f6;
+font-size:16px;
+font-weight:600;
+margin:20px;
 `
 const StoreInfTop = styled.div`
 display:flex;
@@ -112,24 +142,111 @@ justify-content:space-between;
 const StoreName = styled.div`
 display:flex;
 align-items:center;
+color:#5E5968;  
+margin:18px;
+font-weight:700;
+font-size:17px;
 `
 const StoreMinPrice = styled.div`
+display:flex;
+margin:18px;
+font-size:15px;
+font-weight:500;
 `
 const MenuInf = styled.div`
 `
 
 
 const CartPrice = styled.div`
+display:flex;
+flex-direction:column;
+padding:20px;
+
 `
-const MinPrice = styled.div`
-`
-const TotalPriceButton = styled.button`
-`
+
+
 const OrderPrice = styled.div`
+font-size:17px;
+font-weight:500;
+color:#505967;
+
+`
+const OrderPriceContainer =styled.div`
+display: flex;
+flex-direction: row;
+justify-content:space-between
+`
+const OrderPriceText =styled.div`
+font-size:17px;
+font-weight:500;
+color:#8b95a1;
 `
 const DeliveryFee = styled.div`
+font-size:17px;
+font-weight:500;
+color:#505967;
+margin-top:10px;
 `
-const CartBottom = styled.div`
+const DeliveryFeeContainer =styled.div`
+display: flex;
+flex-direction: row;
+justify-content:space-between
+
 `
+const DeliveryFeeText = styled.div`
+font-size:17px;
+font-weight:500;
+color:#8b95a1;
+margin-top:10px;
+`
+
 const CartTotalPrice = styled.div`
+font-size:17px;
+font-weight:600;
+color:#4E5968;
+margin-top:16px;
+
+`
+const CartTotalPriceContainer = styled.div`
+display: flex;
+flex-direction: row;
+justify-content:space-between
+`
+const CartTotalPriceText =styled.div`
+font-size:17px;
+font-weight:500;
+color:#4E5968;
+margin-top:16px;
+
+`
+
+const CartBottom = styled.div`
+position:fixed;
+bottom:0;
+
+`
+const CartBottomContainer = styled.div`
+display:flex;
+justify-content:center;
+
+`
+const TotalPriceButton = styled.button`
+border-radius: 12px;
+background-color: #3182f6;
+cursor: pointer;
+font-size:16px;
+font-weight:600;
+color:#ffffff;
+border:none;
+width:350px;
+height:56px;
+margin:10px;
+`
+
+const MinPrice = styled.div`
+font-size:17px;
+font-weight:500;
+color:#6b7684;
+padding:10px;
+margin-left:80px;
 `
